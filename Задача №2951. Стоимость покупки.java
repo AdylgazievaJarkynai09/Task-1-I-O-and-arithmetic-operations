e5a2234860efc842066792e0ccb55e47d072d9cb
AdylgazievaJarkynai09 <satosugucanon6@gmail.com>
2026-09-22 13:20:53 +0600

    Create Задача №2951. Стоимость покупки.java

---
+++ Задача №2951. Стоимость покупки.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int n = scanner.nextInt();
+        
+        int totalKopecks = (a * 100 + b) * n;
+        
+        int rubles = totalKopecks / 100;
+        int kopecks = totalKopecks % 100;
+        
+        System.out.println(rubles + " " + kopecks);
+    }
+}
