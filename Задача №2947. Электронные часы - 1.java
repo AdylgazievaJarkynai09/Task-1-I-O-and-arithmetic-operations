5be7ac7f72b31215e41b0fd84049c7dee1a8b20d
AdylgazievaJarkynai09 <satosugucanon6@gmail.com>
2026-09-22 12:58:22 +0600

    Create Задача №2947. Электронные часы - 1.java

---
+++ Задача №2947. Электронные часы - 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        int totalMinutes = n % 1440;
+        
+        int hours = totalMinutes / 60;
+        int minutes = totalMinutes % 60;
+        
+        System.out.println(hours + " " + minutes);
+    }
+}
