7b3e0bc2537c97cda838b42078aa24ab699262b7
AdylgazievaJarkynai09 <satosugucanon6@gmail.com>
2026-09-22 13:36:06 +0600

    Create Задача №2955. Улитка.java

---
+++ Задача №2955. Улитка.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int h = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        int step = a - b;
+        int diff = h - a;
+        
+        int day = -Math.floorDiv(-diff, step) + 1;
+        
+        System.out.println(day);
+    }
+}
